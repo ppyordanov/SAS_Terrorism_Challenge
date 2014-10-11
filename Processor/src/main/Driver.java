099520679dff4010b5bc7b7577a224e2a6703191
georgievm <georgievm7@gmail.com>
2014-10-11 14:40:06 +0100

    modified the driver

--- Processor/src/main/Driver.java
+++ Processor/src/main/Driver.java
@@ -2,6 +2,7 @@ package main;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.json.JSONException;
 
@@ -16,9 +17,9 @@ public class Driver {
 		keywordExtractor.extractAllEvents();
 		for(ArrayList<String> keyword:keywordExtractor){
 			for(int j=1; j<keyword.size(); j++){
-				//create a node here
 				if(keyword.get(j).isEmpty()) continue;
 				ArrayList<String> res = Crawler.getWikiResults(keyword.get(j));
+				System.out.println(Arrays.toString(res.toArray()));
 			}
 		}
 	}
