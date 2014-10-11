8d509dafbede9affa19d58ab1c95c51c1ec0414b
georgievm <georgievm7@gmail.com>
2014-10-11 11:52:34 +0100

    json parser included

--- Processor/src/Crawler.java
+++ Processor/src/Crawler.java
@@ -6,17 +6,22 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 public class Crawler {
 	
 	//TODO: test method, remove later
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException, JSONException{
 		ArrayList<String> list = new ArrayList<String>();
 		list.add("Dark_Avengers");
 		getWikiResults(list);
 	}
 
 	
-	public static void getWikiResults(ArrayList<String> keywords) throws IOException {
+	public static void getWikiResults(ArrayList<String> keywords) throws IOException, JSONException {
 		for(String keyword: keywords){
 		HttpURLConnection connection = null;
 	    try {
@@ -24,10 +29,21 @@ public class Crawler {
 	        connection = (HttpURLConnection) url.openConnection();
 	        connection.connect();
 	        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	        String json = "";
 	        String line = "";
 	        while((line = br.readLine())!=null){
-	        	System.out.println(line);
+	        	json += line;
 	        }
+	        
+	        JSONObject object = new JSONObject(json);
+	        JSONObject q = object.getJSONObject("query");
+	        JSONArray v = q.getJSONArray("search");
+	        for(int i=0; i<v.length(); i++){
+	        	JSONObject j = (JSONObject) v.get(i);
+	        	System.out.println(j.get("title"));
+	        }
+	        
+	        
 	    } catch (MalformedURLException e1) {
 	        e1.printStackTrace();
 	    } catch (IOException e1) {
