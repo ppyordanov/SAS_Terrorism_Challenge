84261d2b4e97c454d520f9a9f4fe1c4e1fd9c50c
Radko Kotev <radkokotev@gmail.com>
2014-10-11 17:39:26 +0100

    Frequency count

--- Processor/src/extractor/KeywordExtractor.java
+++ Processor/src/extractor/KeywordExtractor.java
@@ -1,4 +1,5 @@
 package extractor;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -7,26 +8,78 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
-public class KeywordExtractor implements Iterable<ArrayList<String>>{
-	private static final String[] MONTH_NAME = {"", "January", "February",
+public class KeywordExtractor implements Iterable<ArrayList<String>> {
+	private static final String[] MONTH_NAME = { "", "January", "February",
 			"March", "April", "May", "June", "July", "August", "September",
 			"October", "November", "December" };
 	private static final String FILENAME = "gtd_tabs.csv";
-	private static final String[] IMPORTANT_ATTR = {"eventid", "iyear", "imonth",
-			"country_txt", "city", "location", "target1", "gname"};
+	private static final String[] IMPORTANT_ATTR = { "eventid", "iyear",
+			"imonth", "country_txt", "city", "location", "target1", "gname" };
+	private static String[] STOPWORDS = {"", "a", "about", "above", "above",
+			"across", "after", "afterwards", "again", "against", "all",
+			"almost", "alone", "along", "already", "also", "although",
+			"always", "am", "among", "amongst", "amoungst", "amount", "an",
+			"and", "another", "any", "anyhow", "anyone", "anything", "anyway",
+			"anywhere", "are", "around", "as", "at", "back", "be", "became",
+			"because", "become", "becomes", "becoming", "been", "before",
+			"beforehand", "behind", "being", "below", "beside", "besides",
+			"between", "beyond", "bill", "both", "bottom", "but", "by", "call",
+			"can", "cannot", "cant", "co", "con", "could", "couldnt", "cry",
+			"de", "describe", "detail", "do", "done", "down", "due", "during",
+			"each", "eg", "eight", "either", "eleven", "else", "elsewhere",
+			"empty", "enough", "etc", "even", "ever", "every", "everyone",
+			"everything", "everywhere", "except", "few", "fifteen", "fify",
+			"fill", "find", "fire", "first", "five", "for", "former",
+			"formerly", "forty", "found", "four", "from", "front", "full",
+			"further", "get", "give", "go", "had", "has", "hasnt", "have",
+			"he", "hence", "her", "here", "hereafter", "hereby", "herein",
+			"hereupon", "hers", "herself", "him", "himself", "his", "how",
+			"however", "hundred", "ie", "if", "in", "inc", "indeed",
+			"interest", "into", "is", "it", "its", "itself", "keep", "last",
+			"latter", "latterly", "least", "less", "ltd", "made", "many",
+			"may", "me", "meanwhile", "might", "mill", "mine", "more",
+			"moreover", "most", "mostly", "move", "much", "must", "my",
+			"myself", "name", "namely", "neither", "never", "nevertheless",
+			"next", "nine", "no", "nobody", "none", "noone", "nor", "not",
+			"nothing", "now", "nowhere", "of", "off", "often", "on", "once",
+			"one", "only", "onto", "or", "other", "others", "otherwise", "our",
+			"ours", "ourselves", "out", "over", "own", "part", "per",
+			"perhaps", "please", "put", "rather", "re", "same", "see", "seem",
+			"seemed", "seeming", "seems", "serious", "several", "she",
+			"should", "show", "side", "since", "sincere", "six", "sixty", "so",
+			"some", "somehow", "someone", "something", "sometime", "sometimes",
+			"somewhere", "still", "such", "system", "take", "ten", "than",
+			"that", "the", "their", "them", "themselves", "then", "thence",
+			"there", "thereafter", "thereby", "therefore", "therein",
+			"thereupon", "these", "they", "thickv", "thin", "third", "this",
+			"those", "though", "three", "through", "throughout", "thru",
+			"thus", "to", "together", "too", "top", "toward", "towards",
+			"twelve", "twenty", "two", "un", "under", "unknown", "until", "up", "upon",
+			"us", "very", "via", "was", "we", "well", "were", "what",
+			"whatever", "when", "whence", "whenever", "where", "whereafter",
+			"whereas", "whereby", "wherein", "whereupon", "wherever",
+			"whether", "which", "while", "whither", "who", "whoever", "whole",
+			"whom", "whose", "why", "will", "with", "within", "without",
+			"would", "yet", "you", "your", "yours", "yourself", "yourselves",
+			"the" };
+	private static final TreeSet<String> STOPWORDS_SET = new TreeSet<String>(
+			Arrays.asList(STOPWORDS));
 	private static final String[] TEXT_ATTR = { "summary", "motive", "addnotes" };
 	private static final String DELIMINATOR = "\t";
 	private static final String SAMPLE_EVENT_ID = "197001020002";
+
 	private TreeMap<String, Integer> attributeIndices;
 	private TreeMap<String, ArrayList<String>> eventKeywords;
 	private TreeMap<String, ArrayList<String>> relatedEvents;
+	private TreeMap<String, TreeMap<String, Integer>> freqCount;
 	private BufferedReader scanner;
 
 	public KeywordExtractor() {
 		this(FILENAME);
 	}
-	
+
 	public KeywordExtractor(String filename) {
 		try {
 			scanner = new BufferedReader(new FileReader(filename));
@@ -37,6 +90,7 @@ public class KeywordExtractor implements Iterable<ArrayList<String>>{
 		attributeIndices = new TreeMap<String, Integer>();
 		eventKeywords = new TreeMap<String, ArrayList<String>>();
 		relatedEvents = new TreeMap<String, ArrayList<String>>();
+		freqCount = new TreeMap<String, TreeMap<String, Integer>>();
 	}
 
 	public void extractAllEvents() throws IOException {
@@ -45,19 +99,42 @@ public class KeywordExtractor implements Iterable<ArrayList<String>>{
 		for (String h : header) {
 			attributeIndices.put(h, index++);
 		}
-		for (String line = scanner.readLine(); line != null && !line.isEmpty(); line = scanner.readLine().trim()) {
+		for (String line = scanner.readLine(); line != null && !line.isEmpty(); line = scanner
+				.readLine().trim()) {
 			ArrayList<String> keywords = new ArrayList<String>();
 			ArrayList<String> events = new ArrayList<String>();
-			String eventId = extractSingleEvent(line, keywords, events);
-//			System.out.println(eventId);
+			TreeMap<String, Integer> freq = new TreeMap<String, Integer>();
+			String eventId = extractSingleEvent(line, keywords, events, freq);
+			// System.out.println(eventId);
 			eventKeywords.put(eventId, keywords);
 			relatedEvents.put(eventId, events);
+			freqCount.put(eventId, freq);
 		}
 		scanner.close();
 	}
 
-	private String extractSingleEvent(String line,
-			ArrayList<String> keywords, ArrayList<String> relatedEvents) {
+	public static boolean isStopWord(String word) {
+		return STOPWORDS_SET.contains(word.toLowerCase());
+	}
+	
+	private void getWordFrequency(String line, TreeMap<String, Integer> freq) {
+		String[] items = line.split("[\\p{P} \\t\\n\\r]");
+		for (int i = 0; i < items.length; ++i) items[i] = items[i].toLowerCase();
+		for (String item : items) {
+			if (isStopWord(item)) continue;
+			if (!item.matches("[a-zA-Z]{3,}")) continue;
+			Integer count = freq.get(item);
+			if (count == null) {
+				count = 0;
+			}
+			count++;
+			freq.put(item, count);
+		}
+	}
+
+	private String extractSingleEvent(String line, ArrayList<String> keywords,
+			ArrayList<String> relatedEvents, TreeMap<String, Integer> freq) {
+		getWordFrequency(line, freq);
 		String[] items = line.split(DELIMINATOR);
 		for (String attr : IMPORTANT_ATTR) {
 			Integer index = attributeIndices.get(attr);
@@ -65,11 +142,12 @@ public class KeywordExtractor implements Iterable<ArrayList<String>>{
 				System.err.println("Unexpected attribute " + attr);
 				System.exit(1);
 			}
-			if (index >= items.length) System.err.println(Arrays.toString(items) + " line = " + line);
+			if (index >= items.length)
+				System.err.println(Arrays.toString(items) + " line = " + line);
 			if (!items[index].isEmpty()) {
 				if (attr.equals("imonth")) {
 					keywords.add(MONTH_NAME[Integer.parseInt(items[index])]);
-				} else {
+				} else if (!isStopWord(items[index])) {
 					keywords.add(items[index]);
 				}
 			}
@@ -85,12 +163,19 @@ public class KeywordExtractor implements Iterable<ArrayList<String>>{
 						if (word.isEmpty()) {
 							continue;
 						}
+						if (isStopWord(word)) {
+							if (!keyword.isEmpty() && keyword.contains(" ")) {
+								keywords.add(keyword);
+								keyword = "";
+							}
+							continue;
+						}
 						if (Character.isUpperCase(word.charAt(0))) {
 							if (!keyword.isEmpty()) {
 								keyword += " ";
 							}
 							keyword += word;
-						} else if (!keyword.isEmpty()) {
+						} else if (!keyword.isEmpty() && keyword.contains(" ")) {
 							keywords.add(keyword);
 							keyword = "";
 						}
@@ -105,13 +190,17 @@ public class KeywordExtractor implements Iterable<ArrayList<String>>{
 		}
 		return items[0];
 	}
-	
+
 	public ArrayList<String> getKeywords(String eventId) {
 		return eventKeywords.get(eventId);
 	}
-	
+
 	public ArrayList<String> getEvents(String eventId) {
 		return relatedEvents.get(eventId);
+	}
+
+	public TreeMap<String, Integer> getFreqCount(String eventId) {
+		return freqCount.get(eventId);
 	}
 
 	@Override
--- Processor/src/extractor/KeywordExtractorTest.java
+++ Processor/src/extractor/KeywordExtractorTest.java
@@ -1,6 +1,7 @@
 package extractor;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Map.Entry;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -18,16 +19,39 @@ public class KeywordExtractorTest {
 
 	@Test
 	public void testKeywords() throws IOException {
-		String[] expected1 = {"197001000003", "1970", "January", "Japan", "Fukouka", "U.S. Consulate", "Unknown"};
-		String[] expected2 = {"197001080001", "1970", "January", "Italy", "Rome", "Flight 802 Boeing 707", "Unknown"};
+		String[] expected1 = {"197001000003", "1970", "January", "Japan", "Fukouka", "U.S. Consulate"};
+		String[] expected2 = {"197001080001", "1970", "January", "Italy", "Rome", "Flight 802 Boeing 707"};
 		Assert.assertArrayEquals(expected1, extractor.getKeywords("197001000003").toArray());
 		Assert.assertArrayEquals(expected2, extractor.getKeywords("197001080001").toArray());
+		System.out.println(Arrays.toString(extractor.getKeywords("197003140004").toArray()));
 	}
 	
 	@Test
 	public void testRelatedEvents() throws IOException {
 		String[] expected = {"197001020003", "197008240001"};
 		Assert.assertArrayEquals(expected, extractor.getEvents("197001030001").toArray());
+	}
+	
+	@Test
+	public void testFrequencyCount() throws IOException {
+		String[][] expected = {
+				{"armstrong", "5"},
+				{"august", "2"},
+				{"bombing", "2"},
+				{"building", "2"},
+				{"floor", "2"},
+				{"gang", "2"},
+		};
+		String[][] actual = {{"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}};
+		int rows = 0;
+		for (Entry<String, Integer> e : extractor.getFreqCount("197001030001").entrySet()) {
+			if (e.getValue() <= 1) continue;
+			if (++rows > 6) break;
+			actual[rows - 1][0] = e.getKey();
+			actual[rows - 1][1] = e.getValue().toString();
+		}
+		for (int i = 0; i < expected.length; i++)
+			Assert.assertArrayEquals(expected[i], actual[i]);
 	}
 
 }
