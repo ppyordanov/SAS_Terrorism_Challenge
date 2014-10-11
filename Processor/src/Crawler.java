54fe944448fd26557a0499cfce188a731eb9cc29
georgievm <georgievm7@gmail.com>
2014-10-11 10:55:55 +0100

    added a wiki wrapper and method skeletons

--- Processor/src/Crawler.java
+++ Processor/src/Crawler.java
@@ -1,6 +1,22 @@
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Crawler {
+	
+	//TODO: test method, remove later
+	public static void main(String[] args) throws IOException{
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("NATO");
+		getWikiResults(list);
+	}
 
-	
-	
+	public static void getWikiResults(ArrayList<String> keywords) throws IOException {
+		for (String keyword : keywords) {
+			Wiki wiki = new Wiki();
+			String s = wiki.getRenderedText(keyword);
+			System.out.println(s);
+		}
+		
+	}
+
 }
