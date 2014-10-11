437a4d5d9ffbb9e292aafef67e869c8e5a7ace4d
georgievm <georgievm7@gmail.com>
2014-10-11 14:17:45 +0100

    clean up and driver running

--- Processor/src/Driver.java
+++ Processor/src/Driver.java
@@ -1,16 +1,17 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONException;
 
 
 public class Driver {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException, JSONException{
 		KeywordExtractor keywordExtractor = new KeywordExtractor("globalterrorismdb_0814dist.txt");
 		keywordExtractor.extractAllEvents();
 		for(ArrayList<String> i:keywordExtractor){
-			for(int j=1; j<i.size(); j++){
-				
-			}
+//			HashMap<String, ArrayList<String>> res = Crawler.getWikiResults( (ArrayList<String>) i.subList(1, i.size()));
 		}
 		
 		
