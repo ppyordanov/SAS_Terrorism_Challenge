34220d852ae70cee47b87632ef387a0982e61a7d
unknown <you@example.com>
2014-10-11 12:44:28 +0100

    change

--- src/Graph.java
+++ src/Graph.java
@@ -9,24 +9,20 @@ import java.util.Queue;
 public class Graph {
 
 	private ArrayList<Entity> data;
-	private int numVertices = 0;
 
 	/**
 	 creates a new instance of Graph with n vertices
 	*/
 	public Graph(int n) {
 		this.data = new ArrayList<Entity>(n);
-		this.numVertices = n;
 	}
 	
 	public Graph(){
 		this.data = new ArrayList<Entity>();
-		this.numVertices = 0;
 	}
 	
 	//copy constructor
 	public Graph(ArrayList<Entity> input) {
-		this.numVertices = input.size();
 		this.data = input;
 	}
 	
@@ -40,14 +36,6 @@ public class Graph {
 
 	public void setData(ArrayList<Entity> data) {
 		this.data = data;
-	}
-
-	public int getNumVertices() {
-		return numVertices;
-	}
-
-	public void setNumVertices(int numVertices) {
-		this.numVertices = numVertices;
 	}
 	
 	public void addEdge(Entity x, Entity y){
