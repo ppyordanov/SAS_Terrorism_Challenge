fe4636095933fc3cb2b47fe81c1a87c264bfe8bc
georgievm <georgievm7@gmail.com>
2014-10-12 09:41:14 +0100

    introduced multithreading :D , its so fast

--- Processor/src/context/Stats.java
+++ Processor/src/context/Stats.java
@@ -7,10 +7,8 @@ public class Stats {
 	public static double calculateJacard(TreeSet<String> s1, TreeSet<String> s2){
 		TreeSet<String> intersection = new TreeSet<String>(s1);
 		intersection.retainAll(s2);
-		System.out.println("intersection size is "+intersection.size());
 		TreeSet<String> union = new TreeSet<String>(s1);
 		union.addAll(s2);
-		System.out.println("union is "+union.size());
 		return union.size() == 0 ? 0 : intersection.size()*1.0/union.size();
 	}
 	
--- Processor/src/main/Driver.java
+++ Processor/src/main/Driver.java
@@ -1,78 +1,36 @@
 package main;
 
 import extractor.KeywordExtractor;
-import graph.Entity;
-import graph.EntityPair;
 import graph.Graph;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import org.json.JSONException;
 
-import context.Crawler;
-import context.Stats;
 import context.Wiki;
 
-public class Driver {
+public class Driver{
 
-	private static Graph graph;
-	private static KeywordExtractor keywordExtractor;
-	private static Wiki wiki;
+	public static Graph graph;
+	public static KeywordExtractor keywordExtractor;
+	public static Wiki wiki;
+	public static LinkedBlockingQueue<ArrayList<String>> queue;
 
 	public static void main(String[] args) throws IOException, JSONException {
 		wiki = new Wiki();
-		keywordExtractor = new KeywordExtractor("gtd_tabs.csv");
+		keywordExtractor = new KeywordExtractor("globalterrorismdb_0814dist.txt");
 		keywordExtractor.extractAllEvents();
 		graph = new Graph();
+		queue = new LinkedBlockingQueue<ArrayList<String>>();
 		for (ArrayList<String> keyword : keywordExtractor) {
-			processEntry(keyword);
+			queue.add(keyword);
 		}
-		System.out.println(graph.getEntity("197001120001"));
-	}
-
-	public static void processEntry(ArrayList<String> keyword) throws IOException, JSONException {
-		String query = "";
-		for (int j = 1; j < keyword.size(); j++) {
-			if (keyword.get(j).isEmpty())
-				continue;
-			query += " " + keyword.get(j);
-		}
-		query = query.substring(1);
-		query = query.replaceAll(" ", "+");
-		ArrayList<String> res = Crawler.getWikiResults(query);
-		String id = keyword.get(0);
-		String pageTitle = res.get(0);
-		System.err.println(id);
-		Entity entity = Entity.getEntity(id, pageTitle,
-				new ArrayList<Entity>(), keyword, 0.0);
-		String[] categories = wiki.getCategories(pageTitle);
-		if (categories != null && categories.length > 0)
-			for (String category : categories)
-				entity.addCategory(category);
-		graph.addData(entity);
-
-		ArrayList<String> neighbours = new ArrayList<String>();
-		// String[] before = wiki
-		// .whatLinksHere(pageTitle, Wiki.MAIN_NAMESPACE);
-		// for (String s : before)
-		// neighbours.add(s);
-		String[] after = wiki.getLinksOnPage(pageTitle);
-		for (String s : after)
-			neighbours.add(s);
-		for (String s : neighbours) {
-			Entity e = Entity.getEntity(s, s, new ArrayList<Entity>(),
-					new ArrayList<String>(), 0.0);
-			String[] c = wiki.getCategories(s);
-			if (c != null && c.length > 0)
-				for (String s1 : c)
-					e.addCategory(s1);
-			// Weight = Jackard + cos similarity
-			entity.addSimilarPage(new EntityPair(e, Stats.calculateJacard(
-					entity.getCategories(), e.getCategories())
-					+ keywordExtractor.getCosineSimilarity(id,
-							Crawler.getArticleText(s)) * 0.17));
+		for(int i=0; i<100; i++){
+			new Thread(new Process()).start();;
 		}
 	}
+
 
 }
